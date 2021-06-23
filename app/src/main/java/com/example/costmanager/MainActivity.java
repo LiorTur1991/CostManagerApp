acb6fbd787d305ee7e51bf23a97179868c512fd9
LiorTur1991 <59708883+LiorTur1991@users.noreply.github.com>
2021-06-23 21:44:04 +0300

    Fix Html File, Remove Old HTML files

--- app/src/main/java/com/example/costmanager/MainActivity.java
+++ app/src/main/java/com/example/costmanager/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.costmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView mainWebView = findViewById(R.id.MainWebView);
+
+        mainWebView.setWebChromeClient(new WebChromeClient());
         mainWebView.getSettings().setJavaScriptEnabled(true);
         mainWebView.getSettings().setAllowFileAccess(true);
+        mainWebView.getSettings().setDomStorageEnabled(true);
         mainWebView.getSettings().setAllowContentAccess(true);
         mainWebView.loadUrl("file:///android_asset/htmlfiles/home.html");
     }
