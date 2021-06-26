ca3cebd7e7cd9bf62e828840834180a017aac438
LiorTur1991 <59708883+LiorTur1991@users.noreply.github.com>
2021-06-26 13:07:46 +0300

    Small Fixes

--- app/src/main/assets/htmlfiles/costmanager.js
+++ app/src/main/assets/htmlfiles/costmanager.js
@@ -63,3 +63,51 @@ window.costManager.updateListView = function(items){
     $("#list").html(itemsList);
     $("#list").trigger('create');
 }
+
+window.costManager.applySort = function(){
+    let sortMonth = parseInt(document.getElementById('sortMonth').value);
+    window.costManager.updateListView(costManager.getCostsPerMonth(sortMonth));
+};
+
+window.costManager.getCurrentMonthString = function (currentMonth){
+    let monthText;
+    switch (currentMonth){
+        case 0:
+            monthText = "January";
+            break;
+        case 1:
+            monthText = "February";
+            break;
+        case 2:
+            monthText = "March";
+            break;
+        case 3:
+            monthText = "April";
+            break;
+        case 4:
+            monthText = "May";
+            break;
+        case 5:
+            monthText = "June";
+            break;
+        case 6:
+            monthText = "July";
+            break;
+        case 7:
+            monthText = "August";
+            break;
+        case 8:
+            monthText = "September";
+            break;
+        case 9:
+            monthText = "October";
+            break
+        case 10:
+            monthText = "November";
+            break;
+        case 11:
+            monthText = "December";
+            break;
+    }
+    return monthText;
+}
