06c2ab742a3b377e6dbd3184f5fade0af5176b4d
LiorTur1991 <59708883+LiorTur1991@users.noreply.github.com>
2021-06-27 22:54:16 +0300

    Adding API documantation

--- app/src/main/assets/htmlfiles/costmanager.js
+++ app/src/main/assets/htmlfiles/costmanager.js
@@ -1,9 +1,14 @@
 window.costManager = {};
 
-window.costManager.clearData = function(){
-    let data = localStorage.getItem("data");
-}
-
+/**
+ * Creates Item from raw data
+ * @param title
+ * @param description
+ * @param date
+ * @param price
+ * @param note
+ * @returns {{}}
+ */
 window.costManager.getCostItem = function(title, description, date, price, note){
     let costItem = {};
     costItem.title = title;
@@ -12,14 +17,23 @@ window.costManager.getCostItem = function(title, description, date, price, note)
     costItem.price = price;
     costItem.note = note;
     return costItem;
-}
+};
 
+/**
+ * Adds new Item to local-storage data
+ * @param ob - Item
+ */
 window.costManager.addItem = function(ob) {
     let data = costManager.getItems();
     data.push(ob)
     let dataString = JSON.stringify(data);
     localStorage.setItem("data", dataString);
 };
+
+/**
+ * Retrieves all Items in local-storage
+ * @returns {*[]|any} - if void returns empty array
+ */
 window.costManager.getItems = function() {
     let data = localStorage.getItem("data");
     if(data)
@@ -28,27 +42,52 @@ window.costManager.getItems = function() {
         data = [];
         return data;
     }
+};
+
+/**
+ * Wipes all current data in local-storage
+ */
+window.costManager.clearLocalStorageData = function () {
+    let data = [];
+    let dataString = JSON.stringify(data);
+    localStorage.setItem("data",dataString);
+    window.costManager.updateListView(window.costManager.getItems());
+    window.costManager.createPieChart("sortedContainer",-1)
+    $("#pop-up-clearData").popup("open");
+
 }
 
+/**
+ * Retrieves all or some Items, conditioned by user's choice
+ * @param monthNumber
+ * @returns {*[]} - Array of Items
+ */
 window.costManager.getCostsPerMonth = function(monthNumber) {
     let data = localStorage.getItem("data");
     let vec = JSON.parse(data);
     let result = [];
-    if(monthNumber < 0){
+    if(vec.length > 0) {
+        if (monthNumber < 0) {
+            vec.forEach(
+                function (ob) {
+                    result.push(ob);
+                });
+            return result;
+        }
         vec.forEach(
             function (ob) {
-                result.push(ob);
+                let obDateFormat = new Date(ob.date);
+                if (obDateFormat.getMonth() === monthNumber)
+                    result.push(ob);
             });
-        return result;
     }
-    vec.forEach(
-        function (ob) {
-            let obDateFormat = new Date(ob.date);
-            if(obDateFormat.getMonth() === monthNumber)
-                result.push(ob);
-    });
     return result;
-}
+};
+
+/**
+ * Creates\Updates list view
+ * @param items - Items to be displayed
+ */
 window.costManager.updateListView = function(items){
     let itemsList = "<ul data-role='listview' id='list'>";
     items.forEach(
@@ -62,13 +101,13 @@ window.costManager.updateListView = function(items){
     itemsList+= "</ul>";
     $("#list").html(itemsList);
     $("#list").trigger('create');
-}
-
-window.costManager.applySort = function(){
-    let sortMonth = parseInt(document.getElementById('sortMonth').value);
-    window.costManager.updateListView(costManager.getCostsPerMonth(sortMonth));
 };
 
+/**
+ * Changes month Int value to string
+ * @param currentMonth
+ * @returns {string}
+ */
 window.costManager.getCurrentMonthString = function (currentMonth){
     let monthText;
     switch (currentMonth){
@@ -108,6 +147,128 @@ window.costManager.getCurrentMonthString = function (currentMonth){
         case 11:
             monthText = "December";
             break;
+        default:
+            monthText = "Anytime";
+            break;
+
     }
     return monthText;
+};
+
+/**
+ * Sorts the list view by user's choice
+ */
+window.costManager.applySort = function(){
+    let sortMonth = parseInt(document.getElementById('sortMonth').value);
+    window.costManager.updateListView(costManager.getCostsPerMonth(sortMonth));
+    window.costManager.createPieChart("sortedContainer",sortMonth);
+};
+
+/**
+ * Creates data for Pie Chart
+ * @param data
+ * @returns {*[]}
+ */
+window.costManager.calcPieData = function (data){
+    if(data == undefined) return;
+    let totalCounter = 0;
+    let dataMap = new Map();
+    data.forEach(function (calcData) {
+            if (!dataMap.has(calcData.category))
+                dataMap.set(calcData.category, parseInt(calcData.price))
+            else {
+                let counter = dataMap.get(calcData.category);
+                counter = counter + parseInt(calcData.price);
+                dataMap.set(calcData.category, counter)
+            }
+            totalCounter = totalCounter + parseInt(calcData.price);
+        }
+    )
+    let pieData = [];
+    dataMap.forEach((value, key) => {
+            pieData.push({y: (value/totalCounter)*100,label: key})
+        })
+
+    return pieData;
+}
+/**
+ * Draws and renders Pie Chart
+ * @param pieChartName
+ * @param currentMonth
+ */
+window.costManager.createPieChart = function (pieChartName,currentMonth){
+    let picChartDiv = document.getElementById(pieChartName);
+    while (picChartDiv.lastElementChild) {
+        picChartDiv.removeChild(picChartDiv.lastElementChild);
+    }
+    let data = window.costManager.calcPieData(window.costManager.getCostsPerMonth(currentMonth));
+    let currentMonthText = costManager.getCurrentMonthString(currentMonth);
+    let text = currentMonthText + " Summery";
+    if(data.length > 0) {
+        let chart = new CanvasJS.Chart(pieChartName, {
+            animationEnabled: true,
+            title: {
+                text: text
+            },
+            data: [{
+                type: "pie",
+                startAngle: 240,
+                yValueFormatString: "##0.00\"%\"",
+                indexLabel: "{label} {y}",
+                dataPoints: data
+            }]
+        });
+        chart.render();
+        return;
+    }
+    else{
+        let h3=document.createElement("h3");
+        h3.innerHTML="There is No expenses in "+currentMonthText;
+        picChartDiv.appendChild(h3);
+    }
+}
+
+/**
+ * Checking submission of addItem form
+ */
+window.costManager.handleAddButtonClick =  function (){
+    try {
+        let ob = {};
+        ob.title = document.getElementById("title").value;
+        ob.description = document.getElementById("description").value;
+        ob.date = document.getElementById("date").value;
+        ob.price = document.getElementById("price").value;
+        ob.category = document.getElementById("category").value;
+        if(ob.title=="" || ob.description=="" || ob.date=="" || ob.price=="" || ob.category==""){
+            $("#pop-up-warning").popup("open");
+            return;
+        }
+        window.costManager.addItem(ob);
+        document.getElementById("title").value = "";
+        document.getElementById("description").value = "";
+        document.getElementById("date").value = "";
+        document.getElementById("price").value = "";
+        $("#pop-up-success").popup("open");
+    }
+    catch{
+        $("#pop-up-warning").popup("open");
+    }
+}
+
+/**
+ * Updates reports page data
+ */
+window.costManager.updateReportView = function (){
+    window.costManager.updateListView(window.costManager.getItems());
+    window.costManager.createPieChart("sortedContainer",-1);
+}
+
+/**
+ * Updates home page data
+ */
+window.costManager.updateHomeView = function (){
+    let currentDate = new Date();
+    let CurrentMonth = costManager.getCurrentMonthString(currentDate.getMonth());
+    document.getElementById("currentMonth").innerHTML = "Current Month: " + CurrentMonth;
+    window.costManager.createPieChart("chartContainer", currentDate.getMonth());
 }
--- app/src/main/java/com/example/costmanager/MainActivity.java
+++ app/src/main/java/com/example/costmanager/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.costmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,13 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView mainWebView = findViewById(R.id.MainWebView);
 
+        WebView mainWebView = findViewById(R.id.MainWebView);
         mainWebView.setWebChromeClient(new WebChromeClient());
         mainWebView.getSettings().setJavaScriptEnabled(true);
         mainWebView.getSettings().setAllowFileAccess(true);
         mainWebView.getSettings().setDomStorageEnabled(true);
-        mainWebView.getSettings().setAllowContentAccess(true);
         mainWebView.loadUrl("file:///android_asset/htmlfiles/home.html");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Cancel back press!
+
     }
 }
